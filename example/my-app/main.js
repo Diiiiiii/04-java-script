7399c7ac67b7c7e1e9f42fcd47193a77b1377b4c
Dijana Duran <dijanaduran@gmail.com>
2022-01-19 20:06:28 +0100

    update

---
+++ example/my-app/main.js
@@ -0,0 +1,4 @@
+var firstName;
+firstName= 'Dijana';
+
+console.log(firstName);
---
+++ example/my-app/z1.js
@@ -0,0 +1,21 @@
+var firstName, lastName, price, discount, fullPrice;
+
+
+var firstName ="John";
+var lastName = "Doe";
+
+
+var price = 19.90;
+var discount = 0.10;
+var x fullPrice = (price * 100) / discount;
+
+if (x == 1) {
+   x = 2; 
+} else if (a) {
+   var z = 44;
+} else {	
+    return 0;
+}
+
+
+
