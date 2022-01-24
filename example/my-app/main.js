6746a4496b6a3940f18c57931cc0d79ab0847a1d
Dijana Duran <dijanaduran@gmail.com>
2022-01-24 14:02:04 +0100

    update

--- example/my-app/main.js
+++ example/my-app/main.js
@@ -7,9 +7,10 @@ var firstName = "Anamarija Ksenija Brunhilda Šimić";
 
 console.log("Hello " + firstName);
 console.log("Your name is " + firstName.length + "characters long");
-console.log("Your fantasy name is " + firstName.replaceAll("i", "y"));
+//console.log("Your fantasy name is " + firstName.replaceAll("i", "y"));
 
 var text = "Lorem ipsum dolor sit amet.";
 var length = text.length;
-var sit = text.slice(19, 22);
+//var sit = text.slice(18, 22);
+var sit = text.slice(text.indexOf("sit") + 3)
 console.log(sit);
