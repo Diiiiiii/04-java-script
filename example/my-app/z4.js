779bce8834f452f6752426d3ad2ef0463b9be6b9
Dijana Duran <dijanaduran@gmail.com>
2022-01-25 21:11:09 +0100

    z4.js

---
+++ example/my-app/z4.js
@@ -0,0 +1,16 @@
+var firstName = "Dijana";
+var lastName = "Đuran";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//var x = z === 2? y : 5;
+
+var z = 2;
+var y = 3;
+var x;
+
+if (z === 2) {
+    x = y;
+} else {
+    x = 5;
+}
