de4b6e086e90f41f188c47293fe9a8a58247a3c0
Dijana Duran <dijanaduran@gmail.com>
2022-02-09 14:17:06 +0100

    update 9.2.

--- example/my-web-app/app.js
+++ example/my-web-app/app.js
@@ -6,17 +6,33 @@ Trajanje: 15min (18:45 - 21:00)
 1. Onoga trenutka kada timer dođe do 0, sadržaj u timeru mora biti crvene boje.
 
 2. Onoga trenutka kada timer dođe 10 sekundi prije završetka, sadržaj u timeru mora blicati (na kratko se vidi, na kratko se ne vidi).
- */
+
+1. Ispraviti grešku sa veličinom fonta. Svaki naredni klik na "Start timer" button treba staviti font na početnu vrijednost kao što je bilo kod prvog klika na button.
+
+2. Ispraviti grešku sa stilovima. Svaki naredni klik na "Start timer" button treba prikazati timer onako kako je bio prikazan i kod prvog klika na button.
+
+DZ
+1. Promijeniti logiku klika na "Start timer" button tako da nakon klika button bude disabled.
+
+2. Promijeniti logiku klika na "Start timer" button tako da se sa drugim klikom timer pauzira, a sa narednim klikom opet nastavi odbrojavati.
+
+
+*/
+
+
+
 
 var timerElement = document.getElementById('timer');
 console.log(timerElement.dataset);
-var time = parseInt(timerElement.dataset.startTime);
+
 var timerElementStyle = getComputedStyle(timerElement);
 var fontSize = parseInt(timerElementStyle.fontSize);
 var intervalId = null;
 
 var startButton = document.querySelector('[data-action-start]');
 startButton-addEventListener('click', function () {
+    var time = parseInt(timerElement.dataset.startTime);
+
     if (intervalId !== null) {
         clearInterval(intervalId);
     }
