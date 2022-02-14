3f19ea2397bef58db13fb3fbac6dde543d74c5a0
Dijana Duran <dijanaduran@gmail.com>
2022-02-14 11:51:24 +0100

    update

--- example/practise/z17.js
+++ example/practise/z17.js
@@ -9,11 +9,11 @@ function Character(
      this.home = home;
      this.getOib = function() { return _oib; };
     }
-     /* return {
+     /* return  {
         name: name;
         home: home;
         getOib: function() { return _oib; }
-      }; */
+      };   dobiva se veća sloboda u tome kako struktuirati to što je dostupno u objektu. Pogledaj u knjizi JavaScript Patterns*/
      
     
      //funkcija koja uvijek radi istu stvar -kod svake kreacije Characteraa kreira funkciju koja smao vraća OIB i tu funkciu dodijljujemo našem Characteru pod getOib
@@ -22,7 +22,7 @@ function Character(
  //Primjer prototip funkcije na konstruktorima - (ne ovisi na nečem sto je privatno za tu instancu), dodjeljivanje funkcije na prototip smo dobili da se ta funkcija definira samo jednom i ta funkcija je dostupna svim Characterima koji se kreiraju -opće pristupno
  
  Character.prototype.getInfo = function () {
-     return `${this.name} (${this.getIub()}) is from ${this.home}`;
+     return `${this.name} (${this.getOib()}) is from ${this.home}`;
  }
 
 var spongeBob = new Character("SpongeBob", "Bikini Bottom", "12345678903");
