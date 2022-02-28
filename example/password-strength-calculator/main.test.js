7a1fbe356c219d770edf972ad92c726dde3a9d86
Dijana Duran <dijanaduran@gmail.com>
2022-02-28 10:10:05 +0100

    main.test.js update

--- example/password-strength-calculator/main.test.js
+++ example/password-strength-calculator/main.test.js
@@ -4,6 +4,9 @@ test ("Sanity check", () => {
     expect(1).toBe(1);
 });
 
+test ("Password 1234567 has strength 1", () => {
+    expect(checkPassword("1234567")).toBe(1);
+}); 
 test("Password 12345678 has strength 2", () => {
     expect(checkPassword("12345678")).toBe(2);
 });
